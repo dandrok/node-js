211ba028948316a04fb725cbe5ea3aa23154ac45
dandrok <patryk.stusik@gmail.com>
2026-06-10 21:13:19 +0200

    chore(init): initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http')
+
+
+const server = http.createServer((req, res) => {
+  res.write('server response')
+  res.end()
+})
+
+server.listen((3001), () => {
+  console.log('server is ruuning on port 3001')
+})
