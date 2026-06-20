bc46647802914ebd49d740ace62e91ad22685c6f
dandrok <patryk.stusik@gmail.com>
2026-06-20 21:32:18 +0200

    chore: comment

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require('http')
 
 
 const server = http.createServer((req, res) => {
+  console.log(`methods: ${req.method}, ${req.url}`)
   res.write('server response')
   res.end()
 })
